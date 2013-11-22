5029ff7874cdadc8140bfd7ecb2b038dc4cc37ce
jv <jeff@jeffvier.com>
2013-11-21 16:45:22 -0800

    add replicas param

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,10 +4,11 @@ class elasticsearch::install(
   $install_root      = "/opt",
   $java_provider     = 'package',
   $java_package      = 'java-1.7.0-openjdk',
-  $gateway_type      = 's3',
   $cloud_aws_plugin  = '1.14.0', ## https://github.com/elasticsearch/elasticsearch-cloud-aws
   $allow_restart     = false,
     ## service template options ##
+  $gateway_type      = 's3',
+  $replicas          = 1,
   $detail_status     = true,
   $run_as_user       = 'daemon',
   $ulimit_n          = 32768,
