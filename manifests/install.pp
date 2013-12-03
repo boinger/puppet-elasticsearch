7477620873b571a78523b875e3f29f17e114a61d
jv <jeff@jeffvier.com>
2013-12-03 13:46:33 -0800

    big, nasty refactoring of ES

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,25 +1,41 @@
 class elasticsearch::install(
-  $aws_bucket, ## comment-out this parameter if you don't want to use S3 for the gateway
-  $version           = "0.90.3",
-  $install_root      = "/opt",
-  $java_provider     = 'package',
-  $java_package      = 'java-1.7.0-openjdk',
-  $cloud_aws_plugin  = '1.14.0', ## https://github.com/elasticsearch/elasticsearch-cloud-aws
-  $allow_restart     = false,
+  $role               = "combo",  ## or "master" or "data"
+  $version            = "0.90.7",
+  $install_root       = "/opt",
+  $java_provider      = 'package',
+  $java_package       = 'java-1.7.0-openjdk',
+  $cloud_aws_plugin   = '1.16.0', ## https://github.com/elasticsearch/elasticsearch-cloud-aws
+  $allow_restart      = false,
     ## service template options ##
-  $gateway_type      = 's3',
-  $replicas          = 1,
-  $detail_status     = true,
-  $run_as_user       = 'daemon',
-  $ulimit_n          = 32768,
-  $use_upstart       = true,
-  $es_min_mem        = "2g",
-  $es_max_mem        = "2g",
-  $es_java_opts      = "",
-  $index_buffer_size = "75%",
+  $gateway_type       = 'local',
+  $number_of_shards   = 5,
+  $number_of_replicas = 1,
+  $detail_status      = true,
+  $run_as_user        = 'daemon',
+  $ulimit_n           = 32768,
+  $use_upstart        = true,
+  $es_min_mem         = "2g",
+  $es_max_mem         = "2g",
+  $es_java_opts       = "",
+  $index_buffer_size  = "25%",
+  $flush_threshold_ops = 10000,
+  $tcpcompress         = false,
+  $mlockall            = true,
+  $max_content_length  = '500mb',
 ){
 
   $es_home          = "${install_root}/elasticsearch"
+
+  if $role == 'combo' {
+    $nodemaster = 'true'
+    $nodedata = 'true'
+  } elsif $role == 'master' {
+    $nodemaster = 'true'
+    $nodedata = 'false'
+  } elsif $role == 'data' {
+    $nodemaster = 'false'
+    $nodedata = 'true'
+  }
 
   if $java_provider == 'package' {
     if ! defined(Package[$java_package]) {
