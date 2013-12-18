893149d3b64e34ef14faaa0407fa1e1764a6643e
jv <jeff@jeffvier.com>
2013-12-18 14:19:06 -0800

    comments

--- manifests/install.pp
+++ manifests/install.pp
@@ -11,8 +11,8 @@ class elasticsearch::install(
   $discovery_ec2_host_type = 'private_ip',
   $number_of_shards        = 5,
   $number_of_replicas      = 1,
-  $cluster_node_count      = 2,
-  $cluster_data_node_count = 0,
+  $cluster_node_count      = 2,  # node count, if you are using normal "combined" nodes
+  $cluster_data_node_count = 0,  # data node count, if you're splitting among data, master, and client.
   $detail_status           = true,
   $run_as_user             = 'daemon',
   $ulimit_n                = 32768,
