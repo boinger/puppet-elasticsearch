94f6620b4b2184290cfb0a2ce2575fca98f2bccd
jv <jeff@jeffvier.com>
2013-12-18 14:22:50 -0800

    don't round down

--- manifests/install.pp
+++ manifests/install.pp
@@ -28,9 +28,9 @@ class elasticsearch::install(
 ){
 
   if $cluster_data_node_count > 0 {
-    $half_cluster_data_node_count = inline_template("<%= @cluster_data_node_count.to_i / 2 %>")
+    $half_cluster_data_node_count = inline_template("<%= (@cluster_data_node_count.to_i + 1) / 2 %>")
   } else {
-    $half_cluster_node_count = inline_template("<%= @cluster_node_count.to_i / 2 %>")
+    $half_cluster_node_count = inline_template("<%= (@cluster_node_count.to_i + 1) / 2 %>")
   }
 
   $es_home          = "${install_root}/elasticsearch"
